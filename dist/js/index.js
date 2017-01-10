bd4a4b100914e648902025c86d0f84d3a56cc1e1
zhangjikai <zhangjk1993@gmail.com>
2017-01-10 17:13:59 +0800

    udpate

--- dist/js/index.js
+++ dist/js/index.js
@@ -96,9 +96,26 @@
             '' + "</a>" + text + "</h" + level + ">";
     };
 
-    var originalCodeFun = renderer.code;
-    renderer.code = function (code, language) {
+    //var originalCodeFun = renderer.code;
 
+    var originalCodeFun = function (code, lang) {
+
+
+        if (Setting.highlight == Constants.highlight) {
+            return "<pre><code class='" + lang +
+                "'>" + code + "</code></pre>";
+        }
+
+        /*if (Setting.highlight == Constants.syntaxhigh) {
+            return "<pre  class=' brush: " + lang +
+                "; toolbar: false;'>" + code + "</pre>";
+        }*/
+
+        return "<pre><code class='language-" + lang +
+            "'>" + code + "</code></pre>";
+
+    };
+    renderer.code = function (code, language) {
         switch (language) {
             case "seq":
                 if (Setting.sd) {
